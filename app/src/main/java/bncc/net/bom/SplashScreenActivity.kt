5ae42efe1bcf625f8ad9c93d297d5e2a53f89d75
unknown <fajarmh2@gmail.com>
2022-02-17 21:14:56 +0700

    Setting Manifest, Add SignIn, Done Splashscreen(UI + Activity), Setting themes.xml with no action bar

--- app/src/main/java/bncc/net/bom/SplashScreenActivity.kt
+++ app/src/main/java/bncc/net/bom/SplashScreenActivity.kt
@@ -1,12 +1,21 @@
 package bncc.net.bom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this@SplashScreenActivity, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
     }
 
 }
