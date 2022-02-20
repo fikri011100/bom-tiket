8842b7f491aa1f0a39731534550d2462d8c5baa1
Hafiz Elfia Wedo Putra <hafizelfiawedoputra@gmail.com>
2022-02-21 00:19:06 +0700

    Payment Success (Masih dalam process)

---
+++ app/src/main/java/bncc/net/bom/BookingTicketPaymentSuccessActivity.kt
@@ -0,0 +1,11 @@
+package bncc.net.bom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingTicketPaymentSuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking_ticket_payment_success)
+    }
+}
